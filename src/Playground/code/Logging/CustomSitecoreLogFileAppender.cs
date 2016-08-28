ad398bfdd5666f60c05b129dbf0e88e1e5b194b3
Alan Płócieniak <alan-null@outlook.com>
2016-08-28 21:01:42 +0200

    CustomSitecoreLogFileAppender

---
+++ src/Playground/code/Logging/CustomSitecoreLogFileAppender.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using log4net.Appender;
+using log4net.spi;
+using Sitecore.Configuration;
+
+namespace Sitecore.Playground.Logging
+{
+    public class CustomSitecoreLogFileAppender : SitecoreLogFileAppender
+    {
+        public Level Level
+        {
+            get
+            {
+                var filePath = Path.Combine(Settings.DataFolder, "log4net");
+                if (System.IO.File.Exists(filePath))
+                {
+                    var levelStr = System.IO.File.ReadAllText(filePath);
+                    return LogLevelMap.GetLogLevel(levelStr);
+                }
+                return null;
+            }
+        }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            if (Level != null)
+            {
+                var currentLogLevel = loggingEvent.Level;
+                if (currentLogLevel >= Level)
+                {
+                    base.Append(loggingEvent);
+                }
+            }
+            else
+            {
+                base.Append(loggingEvent);
+            }
+        }
+    }
+}
---
+++ src/Playground/code/Logging/LogLevelMap.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using log4net.spi;
+
+namespace Sitecore.Playground.Logging
+{
+    public static class LogLevelMap
+    {
+        static readonly LevelMap LevelMap = new LevelMap();
+
+        static LogLevelMap()
+        {
+            foreach (FieldInfo fieldInfo in typeof(Level).GetFields(BindingFlags.Public | BindingFlags.Static))
+            {
+                if (fieldInfo.FieldType == typeof(Level))
+                {
+                    LevelMap.Add((Level)fieldInfo.GetValue(null));
+                }
+            }
+        }
+
+        public static Level GetLogLevel(string logLevel)
+        {
+            return string.IsNullOrWhiteSpace(logLevel) ? null : LevelMap[logLevel];
+        }
+    }
+}
