9a205611da3269854df762cbbaf1aa25a618b59f
Alan Płócieniak <alan-null@outlook.com>
2016-09-28 21:48:00 +0200

    Hide Experience Editor Tab

---
+++ src/Playground/code/sitecore/shell/client/Sitecore/Playground/ExperienceEditor/Commands/ShowPlaygroundNavigationBar.js
@@ -0,0 +1,12 @@
+﻿define(["sitecore", "/-/speak/v1/ExperienceEditor/ExperienceEditor.js"], function (Sitecore, ExperienceEditor) {
+    Sitecore.Commands.ShowPlaygroundNavigationBar =
+    {
+        strip: "PlaygroundStrip",
+        canExecute: function (context) {
+            return true;
+        },
+        execute: function (context) {
+            //execute command
+        }
+    };
+});
---
+++ src/Playground/code/sitecore/shell/client/Sitecore/Playground/ExperienceEditor/Pipelines/InitializePageEdit/HidePlaygroundStrip.js
@@ -0,0 +1,26 @@
+﻿define(["sitecore", "/-/speak/v1/ExperienceEditor/ExperienceEditor.js"], function (Sitecore, ExperienceEditor) {
+    return {
+        priority: 1,
+        execute: function (context) {
+            var shouldHide = true; // decision
+            if (shouldHide) {
+                var playgroundstrip = "PlaygroundStrip";
+                context.commands = context.commands.filter(function (e) {
+                    return e.stripId !== playgroundstrip;
+                });
+
+                context.commands = context.commands.filter(function (e) {
+                    if (e.command && e.command.strip && e.command.strip === playgroundstrip) {
+                        return false;
+                    }
+                    return true;
+                });
+
+                var experienceAcceleratorStrip = document.querySelector('[stripid=' + playgroundstrip + ']');
+                if (experienceAcceleratorStrip) {
+                    experienceAcceleratorStrip.hide();
+                }
+            }
+        }
+    };
+});
